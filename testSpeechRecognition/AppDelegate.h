fca16dbabb545f02c0817fc6d40e8d6b8c786576
SotaLee <lisongtao1003@163.com>
2016-08-15 18:24:17 +0800

    init

---
+++ testSpeechRecognition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testSpeechRecognition
+//
+//  Created by lisongtao on 16/8/15.
+//  Copyright © 2016年 lst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testSpeechRecognition/RecordViewController.h
@@ -0,0 +1,16 @@
+//
+//  RecordViewController.h
+//  testSpeechRecognition
+//
+//  Created by lisongtao on 16/8/15.
+//  Copyright © 2016年 lst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface RecordViewController : UIViewController<AVAudioRecorderDelegate>
+
+
+@end
+
